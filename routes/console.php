ad672848f7f2168c0dc2a644cfde1734ff0e84e8
Benjamín Trabanino <43897231+trabanino@users.noreply.github.com>
2026-04-07 21:43:16 -0600

    recordatorios

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Jobs\EnviarRecordatoriosCitasManana;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    app(EnviarRecordatoriosCitasManana::class)->handle();
+})->dailyAt('08:00');
+
