30c3caad573c54776c38752d107b3d4c29c98a68
rmorataya11 <renemorataya11@gmail.com>
2026-04-07 20:24:14 -0600

    rutas API para admin de usuarios y profesionales

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,9 @@ use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\ProfileController;
 
+use App\Http\Controllers\Api\Admin\UsuarioController;
 use App\Http\Controllers\EspecialidadController;
+use App\Http\Controllers\ProfesionalController;
 use App\Http\Controllers\ServicioController;
 
 // RUTAS PÚBLICAS
@@ -17,9 +19,19 @@ Route::post('/auth/login', LoginController::class);
 
 Route::get('/especialidades', [EspecialidadController::class, 'index']);
 Route::get('/servicios', [ServicioController::class, 'index']);
+Route::get('/profesionales', [ProfesionalController::class, 'index']);
 
 // RUTAS PROTEGIDAS
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/profile', ProfileController::class);
     Route::post('/auth/logout', LogoutController::class);
+});
+
+// ADMIN
+Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->group(function () {
+    Route::get('/usuarios', [UsuarioController::class, 'index']);
+    Route::post('/usuarios', [UsuarioController::class, 'store']);
+    Route::put('/usuarios/{user}', [UsuarioController::class, 'update']);
+    Route::delete('/usuarios/{user}', [UsuarioController::class, 'destroy']);
+    Route::post('/usuarios/{user}/roles', [UsuarioController::class, 'syncRoles']);
 });
